09c32b0a9fee6a3d00ebf5762493b4551ba57460
Vic0303 <wmatykiewicz@student.agh.edu.pl>
2023-12-04 01:48:58 +0100

    header_files_update
    package.hpp and types.hpp implementation

--- include/package.hpp
+++ include/package.hpp
@@ -5,5 +5,27 @@
 #ifndef NET_SIM_ZPO_PACKAGE_HPP
 #define NET_SIM_ZPO_PACKAGE_HPP
 
+#include "types.hpp"
+#include <set>
+
+class Package {
+public:
+    Package();
+
+    Package(ElementID);
+
+    Package(Package &&);
+
+    Package &operator=(Package &&);
+
+    ElementID get_id()const;
+
+    ~Package();
+
+private:
+    ElementID ID_;
+    static std::set<ElementID> assigned_IDs;
+    static std::set<ElementID> freed_IDs;
+};
 
 #endif //NET_SIM_ZPO_PACKAGE_HPP

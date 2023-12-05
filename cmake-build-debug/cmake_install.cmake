d4e6b5e4c99e9a952fb94872a6ee3ffcf56dad52
LapsanskiZED2115 <nowobilski@student.agh.edu.pl>
2023-12-05 17:36:56 +0100

    s
    test + coś tam w src/package.cpp

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/wikto/OneDrive/Pulpit/3 semestr/ZPO/Net_Sim_ZPO
+# Install script for directory: C:/Users/kamil/Documents/GitHub/Net_Sim_ZPO
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/wikto/AppData/Local/JetBrains/CLion 2022.3.2/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2023.2.2/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/wikto/OneDrive/Pulpit/3 semestr/ZPO/Net_Sim_ZPO/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/kamil/Documents/GitHub/Net_Sim_ZPO/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- src/package.cpp
+++ src/package.cpp
@@ -2,3 +2,16 @@
 //
 // Created by wikto on 28.11.2023.
 //
+#include "package.hpp"
+
+
+
+Package::Package(){
+
+}
+
+
+
+Package::~Package() {
+
+};

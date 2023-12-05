fe4a4a6077c759b69284fe349bbe406fac63168a
Vic0303 <wmatykiewicz@student.agh.edu.pl>
2023-12-05 17:36:08 +0100

    add_gtest

---
+++ cmake-build-debug/googletest-master/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/wikto/OneDrive/Pulpit/3 semestr/ZPO/Net_Sim_ZPO/googletest-master/googletest
+# Build directory: C:/Users/wikto/OneDrive/Pulpit/3 semestr/ZPO/Net_Sim_ZPO/cmake-build-debug/googletest-master/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

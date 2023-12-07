9681a24d763f10e1490751cde8d536188c6b2f7f
ArthasToZiom <artols@student.agh.edu.pl>
2023-12-07 19:53:27 +0100

    uzupełnienie klas w storage_types.hpp cz.1

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -5,37 +5,33 @@
 #ifndef NET_SIM_ZPO_STORAGE_TYPES_HPP
 #define NET_SIM_ZPO_STORAGE_TYPES_HPP
 
+#include "list"
+#include "package.hpp"
+
+enum class PackageQueueType{
+    FIFO,LIFO
+};
 
 class IPackageStockpile{
 public:
+    virtual void push(Package&& package);
+    virtual bool empty() = 0;
+    virtual std::size_t size() = 0;
 };
 
 
 class IPackageQueue : public IPackageStockpile{
 public:
     virtual Package pop() = 0;
+    virtual PackageQueueType get_queue_type() = 0;
 };
 
 class PackageQueue : IPackageQueue{
 public:
     PackageQueue(PackageQueueType) = int;
 private:
-    PackageQueueuType
+    std::list<Package> queue_;
+    PackageQueueType queue_type;
 };
-
-enum class PackageQueueType{
-    FIFO,LIFO
-};
-
-PackageQueueType t = PackageQueueType::FIFO;
-
-switch(t){
-    case PackageQueueType::FIFO:
-        std::cout << "FIFO" << std::endl;
-        break;
-    case PackageQueueType::LIFO:
-        std::cout << "LIFO" << std::endl;
-        break;
-}
 
 #endif //NET_SIM_ZPO_STORAGE_TYPES_HPP

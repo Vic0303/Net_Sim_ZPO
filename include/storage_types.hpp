ccb58667a6d2dfb790e8324da56982a62449f5d0
ArthasToZiom <artols@student.agh.edu.pl>
2023-12-07 18:57:21 +0100

    definicja klas w storage_types.hpp

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -5,4 +5,36 @@
 #ifndef NET_SIM_ZPO_STORAGE_TYPES_HPP
 #define NET_SIM_ZPO_STORAGE_TYPES_HPP
 
+
+class IPackageStockpile{
+public:
+};
+
+class IPackageQueue : public IPackageStockpile{
+public:
+    virtual Package pop() = 0;
+};
+
+class PackageQueue : IPackageQueue{
+public:
+    PackageQueue(PackageQueueType) = int;
+private:
+    PackageQueueuType
+};
+
+enum class PackageQueueType{
+    FIFO,LIFO
+};
+
+PackageQueueType t = PackageQueueType::FIFO;
+
+switch(t){
+    case PackageQueueType::FIFO:
+        std::cout << "FIFO" << std::endl;
+        break;
+    case PackageQueueType::LIFO:
+        std::cout << "LIFO" << std::endl;
+        break;
+}
+
 #endif //NET_SIM_ZPO_STORAGE_TYPES_HPP

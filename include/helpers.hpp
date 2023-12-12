8cc8ea337ad719f2c72527b84798c97b5228787d
ArthasToZiom <artols@student.agh.edu.pl>
2023-12-12 17:13:59 +0100

    Dodanie szkieletu helpers.cpp i helpers.hpp
    Pozdrawiam

--- include/helpers.hpp
+++ include/helpers.hpp
@@ -5,4 +5,16 @@
 #ifndef NET_SIM_ZPO_HELPERS_HPP
 #define NET_SIM_ZPO_HELPERS_HPP
 
+#include <functional>
+#include <random>
+
+#include "types.hpp"
+
+extern std::random_device rd;
+extern std::mt19937 rng;
+
+extern double default_probability_generator();
+
+extern ProbabilityGenerator probability_generator;
+
 #endif //NET_SIM_ZPO_HELPERS_HPP

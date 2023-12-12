b676386c06851226abdeb6dccf6f7d06f2c092da
Vic0303 <wmatykiewicz@student.agh.edu.pl>
2023-12-12 18:10:09 +0100

    types.hpp implementation, nodes.hpp - IPackageReciever implementation

--- include/types.hpp
+++ include/types.hpp
@@ -2,7 +2,15 @@
 // Created by wikto on 28.11.2023.
 //
 
+
 #ifndef NET_SIM_ZPO_TYPES_HPP
 #define NET_SIM_ZPO_TYPES_HPP
+
+    #include <functional>
+
     using ElementID = int;
+    using TimeOffset = int;
+    using Time = int;
+    using ProbabilityGenerator = std::function<double()>;
+
 #endif //NET_SIM_ZPO_TYPES_HPP

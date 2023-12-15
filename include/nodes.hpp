3bbd09e592df80465764f687bcd61393675175c4
LapsanskiZED2115 <nowobilski@student.agh.edu.pl>
2023-12-15 20:37:15 +0100

    Stworzyłem dzieło trwalsze od spiżu
    Prawie połamałem laptopa na koloanie i rzuciłem studia po tym jak po raz 3032 wywalało mi błąd

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -5,54 +5,66 @@
 #ifndef NET_SIM_ZPO_NODES_HPP
 #define NET_SIM_ZPO_NODES_HPP
 
+
 #include <optional>
 #include "config.hpp"
 #include "storage_types.hpp"
 #include "types.hpp"
 #include "memory"
+#include "package.hpp"
+#include <map>
+#include "helpers.hpp"
 
 class IPackageReceiver{
 public:
     virtual void receive_package(Package &&p) = 0;
-    virtual ElementID get_id() = 0;
+    virtual ElementID get_id() const  = 0;
 
     virtual IPackageStockpile::const_iterator begin() const = 0;
     virtual IPackageStockpile::const_iterator end() const = 0;
     virtual IPackageStockpile::const_iterator cbegin() const = 0;
     virtual IPackageStockpile::const_iterator cend() const = 0;
 
-    #if (defined EXERCISE_ID && EXERCISE_ID != EXERCISE_ID_NODES)
+#if (defined EXERCISE_ID && EXERCISE_ID != EXERCISE_ID_NODES)
     virtual ReceiverType get_receiver_type() const = 0;
-    #endif
+#endif
 
     virtual ~IPackageReceiver() = default;
 };
 
-class Storehouse: public IPackageReceiver {
+class ReceiverPreferences {
 public:
-    Storehouse(ElementID id, std::unique_ptr<IPackageStockpile>
-            d = std::make_unique<PackageQueue>(PackageQueueType::FIFO)) : id_(id), d_(std::move(d)) {}
+    using preferences_t = std::map<IPackageReceiver *, double>;
+    using const_iterator = preferences_t::const_iterator;
 
-    ElementID get_id() override { return id_; }
-    void receive_package(Package &&p) override { d_->push(std::move(p)); }
+    explicit ReceiverPreferences(ProbabilityGenerator pg = probability_generator) : pg_(std::move(pg)) {}
 
-    IPackageStockpile::const_iterator cbegin() const override { return d_->cbegin(); }
-    IPackageStockpile::const_iterator cend() const override { return d_->cend(); }
-    IPackageStockpile::const_iterator begin() const override { return d_->begin(); }
-    IPackageStockpile::const_iterator end() const override { return d_->end(); }
+    const_iterator begin() const { return preferences_.cbegin(); }
+    const_iterator end() const { return preferences_.cend(); }
+    const_iterator cbegin() const { return preferences_.cbegin(); }
+    const_iterator cend() const { return preferences_.cend(); }
 
+
+    void add_receiver(IPackageReceiver *r);
+    void remove_receiver(IPackageReceiver *r);
+    IPackageReceiver *choose_receiver();
+
+    const preferences_t &get_preferences() const { return preferences_; }
 
 private:
-    ElementID id_;
-    std::unique_ptr<IPackageStockpile> d_;
-};
+    preferences_t preferences_;
 
+    ProbabilityGenerator pg_;
+};
 class PackageSender {
 public:
+    ReceiverPreferences receiver_preferences_;
+
+    // konstruktor i konstruktor przenoszący
     // konstruktor i konstruktor przenoszący
     PackageSender() = default;
-    PackageSender(PackageSender &&packageSender) = default;
-
+    //Konstruktor przenoszący PackageSender(PackageSender&&) powinien mieć domyślną implementację
+    PackageSender(PackageSender &&pack_sender) = default;
     void send_package();
     const std::optional<Package> &get_sending_buffer() const { return bufor_; }
 
@@ -66,7 +78,24 @@ private:
     // standardowego std::optional.
     std::optional<Package> bufor_ = std::nullopt;
 };
+class Storehouse: public IPackageReceiver {
+public:
+    Storehouse(ElementID id, std::unique_ptr<IPackageStockpile>
+    d = std::make_unique<PackageQueue>(PackageQueueType::FIFO)) : id_(id), d_(std::move(d)) {}
 
+    ElementID get_id() const override { return id_; }
+    void receive_package(Package &&p) override { d_->push(std::move(p)); }
+
+    IPackageStockpile::const_iterator cbegin() const override { return d_->cbegin(); }
+    IPackageStockpile::const_iterator cend() const override { return d_->cend(); }
+    IPackageStockpile::const_iterator begin() const override { return d_->begin(); }
+    IPackageStockpile::const_iterator end() const override { return d_->end(); }
+
+
+private:
+    ElementID id_;
+    std::unique_ptr<IPackageStockpile> d_;
+};
 class Worker : public IPackageReceiver, public PackageSender {
 public:
     Worker(ElementID id, TimeOffset pd, std::unique_ptr<IPackageQueue> q)
@@ -80,7 +109,7 @@ public:
     void process_buffer();
 
     void receive_package(Package &&p) override {q_->push(std::move(p)); }
-    ElementID get_id() override { return id_; }
+    ElementID get_id() const override { return id_; }
 
     IPackageStockpile::const_iterator cbegin() const override { return q_->cbegin(); }
     IPackageStockpile::const_iterator cend() const override { return q_->cend(); }
@@ -100,7 +129,21 @@ private:
     std::optional<Package> bufor_ = std::nullopt;
 
 };
+class Ramp : public PackageSender {
+public:
+    Ramp(ElementID id, TimeOffset di) : PackageSender(), id_(id), di_(di) {}
 
+    void deliver_goods(Time t);
 
+    TimeOffset get_delivery_interval() const { return di_; }
+
+    ElementID get_id() const { return id_; }
+
+private:
+    ElementID id_;
+    TimeOffset di_;
+    Time t_;
+    std::optional<Package> bufor_ = std::nullopt;
+};
 
 #endif //NET_SIM_ZPO_NODES_HPP
--- src/nodes.cpp
+++ src/nodes.cpp
@@ -10,17 +10,96 @@ void Worker::process_buffer() {
 
 void Worker::do_work(Time t) {
     // Sprawdzenie czy bufor jest pusty i czy kolejka nie jest pusta
-    if (!bufor_ && !q_->empty()) {
+    if (!bufor_ && !q_->empty())
+    {
         bufor_.emplace(q_->pop());
         t_ = t;
     } else {
         // Sprawdzenie czy warunek czasowy jest spełniony
-        if (t - t_ + 1 == pd_) {
+        if (t - t_ + 1 == pd_)
+        {
             process_buffer();
             // Jeżeli kolejka nie jest pusta nowy element umieszczany w buforze
-            if (!q_->empty()) {
+            if (!q_->empty())
+            {
                 bufor_.emplace(q_->pop());
             }
+        }
+    }
+}
+void ReceiverPreferences::add_receiver(IPackageReceiver *r) {
+
+    double numbersss = double(preferences_.size());
+    if (numbersss == 0)
+    {
+        preferences_[r] = 1.0;
+    } else
+    {
+        for (auto &rec: preferences_)
+        {
+            rec.second = 1 / (numbersss + 1);
+        }
+        preferences_[r] = 1 / (numbersss + 1);
+    }
+}
+
+
+
+void ReceiverPreferences::remove_receiver(IPackageReceiver *r) {
+    double numberss = double(preferences_.size());
+    if (numberss > 1) {
+        for (auto &rec: preferences_) {
+            if (rec.first != r) {
+                rec.second = 1 / (numberss - 1);
+            }
+        }
+    }
+    preferences_.erase(r);
+}
+
+IPackageReceiver *ReceiverPreferences::choose_receiver() {
+    auto p = pg_();
+    if (p >= 0 && p <= 1) {
+        double distribution = 0.0;
+        for (auto &rec: preferences_)
+        {
+            distribution = distribution + rec.second;
+            if (distribution < 0 || distribution > 1)
+            {
+                return nullptr;
+            }
+            if (p <= distribution)
+            {
+                return rec.first;
+            }
+        }
+        return nullptr;
+    }
+    return nullptr;
+}
+
+void PackageSender::send_package() {
+    IPackageReceiver *receiver;
+    if (bufor_)
+    {
+        receiver = receiver_preferences_.choose_receiver();
+        receiver->receive_package(std::move(*bufor_));
+        bufor_.reset();
+    }
+}
+
+void Ramp::deliver_goods(Time t) {
+    if (!bufor_)
+    {
+        push_package(Package());
+        bufor_.emplace(id_);
+        t_ = t;
+    }
+    else
+    {
+        if (t - di_ == t_)
+        {
+            push_package(Package());
         }
     }
 }
